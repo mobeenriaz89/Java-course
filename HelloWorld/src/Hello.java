721492474bd3c2cc00eef59ba78d7a373cbee985
mobeenriaz89 <mobeenriaz89@gmail.com>
2017-02-02 00:55:31 +0500

    my message

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Mobeen on 2/1/2017.
+ */
+public class Hello {
+    public static void main(String []arg)
+    {
+        System.out.println("Hello mobeen");
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+		        System.out.println("i am getting it");
+
+    }
+}
